980d91091a589cc56912b33e87117c8491c43f11
Casey Sackett <csackett@gmail.com>
2012-08-20 21:46:42 -0700

    slight change to unpack format for feedback service

--- lib/apn/feedback.rb
+++ lib/apn/feedback.rb
@@ -61,7 +61,8 @@ module APN
       # Unpacking code borrowed from http://github.com/jpoz/APNS/blob/master/lib/apns/core.rb
       while line = socket.gets   # Read lines from the socket
         line.strip!
-        f = line.unpack('N1n1H140')
+        f = line.unpack('N1n1H*')
+        #f = line.unpack('N1n1H140')
         feedback << APN::FeedbackItem.new(Time.at(f[0]), f[2])
       end
 
