89f133a00e5cb4683322e3c155f871af1b927690
Casey Sackett <csackett@gmail.com>
2014-05-27 10:18:16 -0700

    #72104612 - allow running multiple APN senders in a single process

--- lib/apn/tasks.rb
+++ lib/apn/tasks.rb
@@ -1,3 +1,29 @@
+def start_worker
+  require 'apn'
+
+  worker = nil
+
+  begin
+    logger = Logger.new(File.join(Rails.root, 'log', 'apn_sender.log'))
+    worker = APN::Sender.new(:cert_path => ENV['CERT_PATH'],
+                             :environment => ENV['ENVIRONMENT'],
+                             :app => ENV['APP'],
+                             :logger => logger,
+                             :use_enhanced_format => ENV['USE_ENHANCED_FORMAT'],
+                             :verbose => true)
+    worker.logger = logger
+    worker.verbose = true
+    worker.very_verbose = true
+  rescue Exception => e
+    raise e
+    # abort "set QUEUE env var, e.g. $ QUEUE=critical,high rake resque:work"
+  end
+
+  puts "*** Starting worker to send apple notifications in the background from #{worker}"
+
+  worker.work(ENV['INTERVAL'] || 5) # interval, will block
+end
+
 # Slight modifications from the default Resque tasks
 namespace :apn do
   task :setup
@@ -6,38 +32,17 @@ namespace :apn do
 
   desc "Start an APN worker"
   task :sender => :setup do
-    require 'apn'
-
-    worker = nil
-
-    begin
-      logger = Logger.new(File.join(Rails.root, 'log', 'apn_sender.log'))
-      worker = APN::Sender.new(:cert_path => ENV['CERT_PATH'],
-                               :environment => ENV['ENVIRONMENT'],
-                               :app => ENV['APP'],
-                               :logger => logger,
-                               :use_enhanced_format => ENV['USE_ENHANCED_FORMAT'],
-                               :verbose => true)
-      worker.logger = logger
-      worker.verbose = true
-      worker.very_verbose = true
-    rescue Exception => e
-      raise e
-      # abort "set QUEUE env var, e.g. $ QUEUE=critical,high rake resque:work"
-    end
-
-    puts "*** Starting worker to send apple notifications in the background from #{worker}"
-
-    worker.work(ENV['INTERVAL'] || 5) # interval, will block
+    start_worker
   end
 
   desc "Start multiple APN workers. Should only be used in dev mode."
   task :senders do
     threads = []
 
-    ENV['COUNT'].to_i.times do
+    ENV['APPS'].split(' ').each do |app|
       threads << Thread.new do
-        system "rake apn:work"
+        ENV['APP'] = app
+        start_worker
       end
     end
 
